b607b353739cdfea89735c092d8d1735eec604ab
luisarante <arantesluisfernando@gmail.com>
2024-09-03 09:25:15 -0300

    Primeiro commit

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,24 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI {get; set;}
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void ExibirEspecificacoes()
+        {
+            Console.WriteLine($"Número: {Numero}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"IMEI: {IMEI}");
+            Console.WriteLine($"Memória: {Memoria} GB");
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.ExibirEspecificacoes();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "4978", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.ExibirEspecificacoes();
+iphone.InstalarAplicativo("Telegram");
